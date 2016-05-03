88c7a6b1c29a529550a3001e2148906c10fe1c66
bmirona <b.mirona@yahoo.com>
2016-05-03 20:38:01 +0200

    some doc

--- src/scores/BestPlayer.java
+++ src/scores/BestPlayer.java
@@ -4,6 +4,11 @@ public class BestPlayer {
 	private String player;
 	private int score;
 	
+	/**
+	 * Constructs a best player from a score and its nickname.
+	 * @param player The nickname you want to give to the player
+	 * @param score The score you want to give to the player
+	 */
 	public BestPlayer(String n, int s){
 		this.player = n;
 		this.score = s;
@@ -23,6 +28,11 @@ public class BestPlayer {
 		this.score = score;
 	}
 	
+	/**
+	 * Compares the score of the current BestPlayer with another BestPlayer's score
+	 * @param p The other BestPlayer we want to compare to.
+	 * @return 0 if the scores are equals. -1 if the other player's score is better. 1 in any other case.
+	 */
 	public int compareTo(BestPlayer p){
 		if (p.score > this.score)
 			return -1;
